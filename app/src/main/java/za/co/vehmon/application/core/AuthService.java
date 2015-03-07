95a5319e6c4d7f0d58b5e11fd10280e475da9f4b
Renaldobenji <Renaldobenji@gmail.com>
2015-03-07 15:27:43 +0200

    New token is a post
    post

--- app/src/main/java/za/co/vehmon/application/core/AuthService.java
+++ app/src/main/java/za/co/vehmon/application/core/AuthService.java
@@ -2,7 +2,9 @@ package za.co.vehmon.application.core;
 
 import java.util.List;
 
+import retrofit.http.Field;
 import retrofit.http.GET;
+import retrofit.http.POST;
 import retrofit.http.Path;
 import za.co.vehmon.application.services.TokenGenerationResult;
 import za.co.vehmon.application.services.UserDetailContract;
@@ -16,8 +18,8 @@ public interface AuthService {
     @GET(Constants.Http.VEHMON_URL_AUTH_FRAG)
     TokenGenerationResult GetTokenForUser(@Path("userName") String username, @Path("password") String password);
 
-    @GET(Constants.Http.VEHMON_URL_AUTH_RENEW_FRAG)
-    UserTokenValidationResponse RenewToken(@Path("token") String token);
+    @POST(Constants.Http.VEHMON_URL_AUTH_RENEW_FRAG)
+    UserTokenValidationResponse RenewToken(@Field("token") String token);
 
     @GET(Constants.Http.VEHMON_URL_AUTH_GETALLUSERS_FRAG)
     List<UserDetailContract> GetAllUsers(@Path("token") String token);
